e3e677c95dcea6106a72e29f19544a5e069f8104
Ertuğrul Taştemür <ertugrul.tastemur@ogr.sakarya.edu.tr>
2021-03-06 23:17:38 +0300

    6 mart

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri başarıyla eklendi : "+ musteri.Isim);
+        }
+        public void Cikar(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri başarıyla silindi : "+ musteri.Isim);
+        }
+        public void Listele(Musteri[] musteriler)
+        {
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Id);
+                Console.WriteLine(musteri.Isim);
+                Console.WriteLine(musteri.yas);
+                Console.WriteLine(musteri.sehir);
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Isim = "Ahmet Gündoğdu";
+            musteri1.yas = 36;
+            musteri1.sehir = "Bursa";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Isim = "Bengü Tonay";
+            musteri2.yas = 26;
+            musteri2.sehir = "Adana";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.Isim = "Ertuğrul Taştemür";
+            musteri3.yas = 20;
+            musteri3.sehir = "Istanbul";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            MusteriManager islemler = new MusteriManager();
+            islemler.Ekle(musteri1);
+            islemler.Cikar(musteri2);
+            islemler.Listele(musteriler);
+
+        }
+    }
+}
